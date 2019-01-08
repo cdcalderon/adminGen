719879c0184d250519f4a946d2766a0af4b82a96
cdcalderon <cdcalderon@gmail.com>
2019-01-07 23:41:05 -0600

    declare initial menu structure, render test

--- src/app/services/shared/sidebar.service.ts
+++ src/app/services/shared/sidebar.service.ts
@@ -3,6 +3,14 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class SidebarService {
 
+  menu: any = [ {
+    title: '',
+    SubMenu: {
+      title: '',
+      category: ''
+    }
+  }];
+
   constructor() { }
 
 }
